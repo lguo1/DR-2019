9c8c9e5fcd7301decc82e8f13e7f37106ec7bf73
apple <apple@dhcp-130-58-103-217.swarthmore.edu>
2019-02-02 23:45:18 -0500

    ****

--- objects.py
+++ objects.py
@@ -2,12 +2,15 @@ class model:
     def __init__(self, graph, sess, name, softmax=True):
         with graph.as_default():
             with tf.variable_scope(name)):
-                self.input_ph = tf.placeholder(dtype=tf.float32, shape=[None, 1])
+                self.input_ph_0 = tf.placeholder(dtype=tf.float32, shape=[None, 1])
+                self.input_ph_1 = tf.placeholder(dtype=tf.float32, shape=[None, 3, 2])
                 self.output_ph = tf.placeholder(dtype=tf.float32, shape=[None, 1])
 
-                W0 = tf.get_variable(name='W0', shape=[1, 20], initializer=tf.contrib.layers.xavier_initializer())
-                W1 = tf.get_variable(name='W1', shape=[20, 20], initializer=tf.contrib.layers.xavier_initializer())
-                W2 = tf.get_variable(name='W2', shape=[20, 1], initializer=tf.contrib.layers.xavier_initializer())
+                W0_0 = tf.get_variable(name='W0_0', shape=[1, 64], initializer=tf.contrib.layers.xavier_initializer())
+                W1_0 = tf.get_variable(name='W1_0', shape=[64, 64], initializer=tf.contrib.layers.xavier_initializer())
+
+                W0_1 = tf.get_variable(name='W0_1', shape=[, 1], initializer=tf.contrib.layers.xavier_initializer())
+                W1_1 = tf.get_variable(name='W1_1', shape=[20, 1], initializer=tf.contrib.layers.xavier_initializer())
 
                 b0 = tf.get_variable(name='b0', shape=[20], initializer=tf.constant_initializer(0.))
                 b1 = tf.get_variable(name='b1', shape=[20], initializer=tf.constant_initializer(0.))
@@ -15,17 +18,18 @@ class model:
 
                 self.weights = [W0, W1, W2]
                 self.biases = [b0, b1, b2]
-                self.activations = [tf.nn.relu, tf.nn.relu, None]
+                if softmax:
+                    self.activations = [tf.nn.relu, tf.nn.relu, tf.nn.softmax]
+                else:
+                    self.activations = [tf.nn.relu, tf.nn.relu, None]
 
                 layer = self.input_ph
                 for W, b, activation in zip(self.weights, self.biases, self.activations):
                     layer = tf.matmul(layer, W) + b
                     if activation is not None:
                         layer = activation(layer)
-                if softmax:
-                    self.output_pred = tf.exp(layer)/tf.exp(layer).sum()
-                else:
-                    self.output_pred = layer
+
+                self.output_pred = layer
                 self.sess = sess
 
     def predict(self, inputs):
@@ -48,26 +52,28 @@ class model:
 
 class node:
     def __init__(self):
-        self.bets = np.full(3,np.inf)
+        self.bets = np.zeros(3)
         self.turns = -1
         self.is_terminal = False
 
     def deal(self):
         self.cards = np.random.choice(3,2)
+        self.turns += 1
 
     def place(self, arr):
         self.bets[self.turns] = arr
+        self.turns += 1
 
     def A(self):
         if self.turns == 0:
-            return [1,2]
+            return [[1,1],[2,1]]
         elif self.turns == 1:
             if self.bets[0] == 1:
-                return [1,2]
+                return [[1,1],[2,1]]
             else:
-                return [0,2]
+                return [[0,1],[2,1]]
         else:
-            return [0,2]
+            return [[0,1],[2,1]]
 
     def P(self):
         if self.turns == -1:
@@ -76,27 +82,31 @@ class node:
             return (self.turns+1)%2
 
     def is_terminal(self):
-        if self.bets[2] != np.inf:
+        if self.bets[2,1] != 0:
             return True
-        elif self.bets[1] != np.inf:
+        elif self.bets[1] == [0,1]:
+            return False
+        elif self.bets[1] != [2,1]:
+            return True
+        elif self.bets[0] == [2,1]:
             return True
         else:
             return False
 
     def util(self, p):
-        thir = self.bets[2]
-        if third == 0:
-            return [3,0][p]
-        elif third == 2:
+        thd = self.bets[2]
+        if thd == [0,1]:
+            return [0,3][p]
+        elif thd == [2,1]:
             return (p == np.argmax(self.cards))*4
         else:
             sec = self.bets[1]
-            if sec == 1:
+            if sec == [1,1]:
                 return (p == np.argmax(self.cards))*2
-            elif sec == 2:
+            elif sec == [2,1]:
                 return (p == np.argmax(self.cards))*4
             else:
-                return (p == np.argmax(self.cards))*3
+                return [3,0][p]
 
     def I(self, p):
         return (self.cards[p], self.bets)
