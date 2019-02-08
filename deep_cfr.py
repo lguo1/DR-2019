bada6688e5bc18d387088ee5c1b7d2d00730594b
Lizhi Guo <lguo1@swarthmore.edu>
2019-02-08 17:23:49 -0500

    funfun

--- deep_cfr.py
+++ deep_cfr.py
@@ -1,33 +1,30 @@
-import tensorflow as tf
-import numpy as np
-import objects
+from objects import *
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("iter", help="number of iterations", type=int)
-parser.add_argument("trav", help="number of iterations", type=int)
+parser.add_argument("iter", help="number of iterations", type=int, default=10000)
+parser.add_argument("trav", help="number of iterations", type=int, default=100)
+args = parser.parse_args()
 
 def main(iter, trav):
-    B_v = [[],[]]
-    B_s = []
+    B_v = (buffer(), buffer())
+    B_s = buffer()
     weights = []
-    sess = tf.Session()
-    M_r = (model(tf.Graph(), sess, 'p0', True), model(tf.Graph(), sess, 'p1', True))
-    M_s = model(tf.Graph(), sess, 'policy')
+    M_r = (model('p0', True), model('p1', True))
+    M_s = model('state')
     for t in range(iter):
         p = t%2
         p_not = (p+1)%2
         for n in range(trav):
-            collect_samples(node(), p, p_not M_r, B_v, B_s)
+            collect_samples(node(), p, p_not, M_r, B_v, B_s)
         weights.extend([(1+t)/2]*n)
-        M_r[p].train(B_v)
-        # update iteration on p_not
+        M_r[p].train(B_v, weights, 2000, 1000)
     M_s.train(B_s)
     sess.close()
     return
 
 def collect_samples(h, p, p_not, M_r, B_v, B_s):
-    if h.is_terminal():
+    if h.is_terminal:
         return h.util(p)
     elif h.P() == p:
         I = h.I(p)
@@ -37,11 +34,13 @@ def collect_samples(h, p, p_not, M_r, B_v, B_s):
             v_a[a] = collect_samples(h.place(a), p, p_not, M_r, B_v, B_s)
         v_s = np.dot(v_a, sigma)
         d = v_a - v_s
-        B_v[p].append(I + d)
+        B_v[p].add(I,d)
     elif h.P() == p_not:
+        I = h.I(p_not)
         sigma = calculate_strategy(I, M_r[p_not])
-        B_s.append(h.I(p_not) + sigma)
-        a = np.random.choice(h.A(), p=sigma)
+        B_s.add(I, sigma)
+        print(sigma)
+        a = h.A()[np.random.choice(2, p=sigma)]
         return collect_samples(h.place(a), p, p_not, M_r, B_v, B_s)
     else:
         return collect_samples(h.deal(), p, p_not, M_r, B_v, B_s)
@@ -49,7 +48,7 @@ def collect_samples(h, p, p_not, M_r, B_v, B_s):
 def calculate_strategy(I, model):
     sum = 0
     d = model.predict(I)
-    d_plus = np.clip(d, 0)
+    d_plus = np.clip(d, 0, None)
     sum = d_plus.sum()
     if sum > 0:
         return d_plus/sum
--- objects.py
+++ objects.py
@@ -1,15 +1,30 @@
+import tensorflow as tf
+import numpy as np
+
+class buffer:
+    def __init__(self):
+        self.list = [[],[],[],[]]
+        self.size = 0
+    def add(self, input, output):
+        self.list[0].append(input[0])
+        self.list[1].append(input[1])
+        self.list[2].append(output)
+        self.size += 1
+        return self.list
+    def sample(self, indices):
+        return (self.list[0,indices], self.list[1,indices], self.list[2,indices])
 class model:
-    def __init__(self, graph, sess, name, softmax=True):
-        self.sess = sess
-        with graph.as_default():
-            with tf.variable_scope(name)):
+    def __init__(self, name, softmax=True):
+        self.graph = tf.Graph()
+        with self.graph.as_default():
+            with tf.variable_scope(name):
                 self.output_ph = tf.placeholder(dtype=tf.float32, shape=[None, 3])
 
                 self.input_ph0 = tf.placeholder(dtype=tf.float32, shape=[None, 1])
                 W0_0 = tf.get_variable(name='W0_0', shape=[1, 32], initializer=tf.contrib.layers.xavier_initializer())
                 W0_1 = tf.get_variable(name='W0_1', shape=[32, 16], initializer=tf.contrib.layers.xavier_initializer())
                 b0_0 = tf.get_variable(name='b0_0', shape=[32], initializer=tf.constant_initializer(0.))
-                b0_1 = tf.get_variable(name='b0/1', shape=[16], initializer=tf.constant_initializer(0.))
+                b0_1 = tf.get_variable(name='b2_0', shape=[16], initializer=tf.constant_initializer(0.))
                 activations = [tf.nn.relu, None]
                 weights = [W0_0, W0_1]
                 biases = [b0_0, b0_1]
@@ -27,52 +42,57 @@ class model:
                 layer1 = tf.matmul(layer1, W1_0) + b1_0
 
                 layer2 = tf.nn.relu(layer0 + layer1)
-                W2_0 = tf.get_variable(name='W2_0', shape=[16, 3], initializer=tf.contrib.layers.xavier_initializer())
-                b2_0 = tf.get_variable(name='b2', shape=[3], initializer=tf.constant_initializer(0.))
+                W2_0 = tf.get_variable(name='W2_1', shape=[16, 3], initializer=tf.contrib.layers.xavier_initializer())
+                b2_0 = tf.get_variable(name='b2_1', shape=[3], initializer=tf.constant_initializer(0.))
                 if softmax:
                     self.output_pred = tf.nn.softmax(tf.matmul(layer2, W2_0) + b2_0)
                 else:
                     self.output_pred = tf.matmul(layer2, W2_0) + b2_0
+                init_graph = tf.global_variables_initializer()
+        self.sess = tf.Session(graph=self.graph)
+        self.sess.run(init_graph)
 
     def predict(self, inputs):
-        return self.sess.run(self.output_pred, feed_dict={input_ph: inputs})
+        return self.sess.run(self.output_pred, feed_dict={self.input_ph0: [inputs[0]], self.input_ph1: [inputs[1]]})
 
     def train(self, B, weights, N_train, N_batch):
         mse = tf.reduce_mean(0.5 * tf.square(self.output_pred - self.output_ph))
         opt = tf.train.AdamOptimizer().minimize(mse)
-        self.sess.run(tf.global_variables_initializer())
         saver = tf.train.Saver()
         for training_step in range(N_train):
-            pairs = tf.contrib.training.weighted_resample(B, weights, 1/N_batch)
-            _, mse_run = self.sess.run([opt, mse], feed_dict={self.input_ph0: pairs[:,0], self.input_ph1: pairs[:,1], self.output_ph: pairs[:,2]})
+            sample = B.sample(tf.contrib.training.weighted_resample(range(B.size), weights, N_batch/B.size))
+            _, mse_run = self.sess.run([opt, mse], feed_dict={self.input_ph0: sample[0], self.input_ph1: sample[1], self.output_ph: sample[2]})
             if training_step % 1000 == 0:
                 print('{0:04d} mse: {1:.3f}'.format(training_step, mse_run))
                 saver.save(sess, '/saved/%s_%d.ckpt'%(self.name, training_step))
 
 class node:
     def __init__(self):
-        self.bets = np.zeros(3)
+        self.actions = np.zeros(3)
+        self.prog = np.zeros(3)
         self.turns = -1
         self.is_terminal = False
 
     def deal(self):
-        self.cards = np.random.choice(3,2)
+        self.cards = np.random.choice(3,(2,1))
         self.turns += 1
+        return self
 
-    def place(self, arr):
-        self.bets[self.turns] = arr
+    def place(self, action):
+        self.actions[self.turns] = action
         self.turns += 1
+        return self
 
     def A(self):
         if self.turns == 0:
-            return [[1,1],[2,1]]
+            return [1,2]
         elif self.turns == 1:
-            if self.bets[0] == [1,1]:
-                return [[1,1],[2,1]]
+            if self.bets[0] == 1:
+                return [[[1],[1]],[[2],[1]]]
             else:
-                return [[0,1],[2,1]]
+                return [[[0],[1]],[[2],[1]]]
         else:
-            return [[0,1],[2,1]]
+            return [[[0],[1]],[[2],[1]]]
 
     def P(self):
         if self.turns == -1:
@@ -81,28 +101,28 @@ class node:
             return (self.turns+1)%2
 
     def is_terminal(self):
-        if self.bets[2] != [0,0]:
+        if self.bets[2] != 0:
             return True
-        elif self.bets[1] == [0,0]:
+        elif self.bets[1] == 0:
             return False
-        elif self.bets[1] != [2,1]:
+        elif self.bets[1] != 2:
             return True
-        elif self.bets[0] == [2,1]:
+        elif self.bets[0] == 2:
             return True
         else:
             return False
 
     def util(self, p):
         thd = self.bets[2]
-        if thd == [0,1]:
+        if thd == [[0],[1]]:
             return [0,3][p]
-        elif thd == [2,1]:
+        elif thd == [[2],[1]]:
             return (p == np.argmax(self.cards))*4
         else:
             sec = self.bets[1]
-            if sec == [1,1]:
+            if sec == [[1],[1]]:
                 return (p == np.argmax(self.cards))*2
-            elif sec == [2,1]:
+            elif sec == [[2],[1]]:
                 return (p == np.argmax(self.cards))*4
             else:
                 return [3,0][p]
