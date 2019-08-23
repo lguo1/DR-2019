38da318d95cb7bd735b8991c6ec5e1614930e184
apple <apple@apples-MacBook-Air.local>
2019-08-23 12:34:49 -0400

    commit

--- games.py
+++ games.py
@@ -2,4 +2,29 @@ import numpy as np
 class Chicken:
     def __init__(self):
         self.u = np.array([[[0,2],[7,6]],[[0,2],[7,6]]])
-        self.shape = (2,2)
+        self.shape = self.u.shape[1:]
+
+class Prison:
+    def __init__(self):
+        self.u = np.array([[[-1,0],[-3,-2]],[[-1,0],[-3,-2]]])
+        self.shape = self.u.shape[1:]
+
+class Sexes:
+    def __init__(self):
+        self.u = np.array([[[4,0],[0,1]],[[1,0],[0,4]]])
+        self.shape = self.u.shape[1:]
+
+class RPS:
+    def __init__(self):
+        self.u = np.array([[[0,1,-1],[-1,0,1],[1,-1,0]],[[0,1,-1],[-1,0,1],[1,-1,0]]])
+        self.shape = self.u.shape[1:]
+
+class RPS2:
+    def __init__(self):
+        self.u = np.array([[[0,5,-5],[-5,0,5],[5,-5,0]],[[0,1,-1],[-1,0,1],[1,-1,0]]])
+        self.shape = self.u.shape[1:]
+
+class RPS3:
+    def __init__(self):
+        self.u = np.array([[[0,100,50],[50,0,100],[100,50,0]],[[0,1,-1],[-1,0,1],[1,-1,0]]])
+        self.shape = self.u.shape[1:]
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from games import *
 from scipy.linalg import null_space as null
 
 def diff(arr):
+    # this function takes an array arr where arr[j] = u^i(ej,a^{-i})
+    # it returns a matrix D such that D[k,j] = u^i(ek,a^{-i}) - u^i(ej,a^{-i})
     l = arr.size
     D = np.zeros((l,l))
     for j in range(l-1):
@@ -10,12 +12,31 @@ def diff(arr):
     return D - np.transpose(D)
 
 def g(ui):
+    # this function takes a matrix ui that represents the utility function of player i
+    # ui is so set up that the row player is player -i and the column player player i
+    # the function returns a 3darray G where G[k,j,y] = u^i(ek,ey) - u^i(ej,ey)
     G = []
     for y in range(ui.shape[0]):
         G.append(diff(ui[y]))
     return np.swapaxes(np.array(G),0,2)
 
+def marginal(Z2):
+    # this function takes a joint distribution matrix and returns the product of its marginals.
+    z1 = np.sum(Z2, axis=1)[None]
+    z2 = np.sum(Z2, axis=0)[None]
+    return np.matmul(z1.T,z2)
+
+def prob(nullspace):
+    # this function takes a n x m 2darray representing the nullspace of some linear equations.
+    # each column in the array is a basis vector.
+    # the function returns an n x 1 array which is a probability distribution and belongs to the nullspace.
+    nullspace[:,np.all(nullspace<=0,axis=0)] *= -1
+    nullspace = nullspace[:,np.logical_not(np.any(nullspace<0,axis=0))]
+    p = np.sum(nullspace,axis=1)
+    return p/np.sum(p)
+
 def CRM(game, T):
+    # this function runs a Universal Calibrated Regret Matching procedure as described in the paper.
     G1 = g(game.u[0])
     G2 = g(game.u[1])
     Z2 = np.zeros(game.shape)
@@ -25,33 +46,57 @@ def CRM(game, T):
     for t in range(1,T+1):
         L1 = np.sum(Z2*G1,axis=2)/t
         L2 = np.sum(np.transpose(Z2)*G2,axis=2)/t
-
-        print(t)
-        print("Z2\n",Z2)
-        print("G2\n",G2)
-        print("Z1*G2\n",np.transpose(Z2)*G2)
-        print("L2\n",L2)
+        # the expected result is Li[k,j] = sum(prob(ej,a^{-i})[u^i(ek,a^{-i}) - u^i(ej,a^{-i}])
+        # over all a^{-i}s.
         L1[L1<0]=0
         L2[L2<0]=0
+        # Li is the lambda matrix described in the paper. It basically is
+        # the nonpositive portion of the correlated regret matrix.
         D1 = np.diagflat(np.sum(L1,axis=0))
         D2 = np.diagflat(np.sum(L2,axis=0))
-        q1 = np.sum(null(L1-D1),axis=1)
-        q1 /= np.sum(q1)
-        q2 = np.sum(null(L2-D2),axis=1)
-        q2 /= np.sum(q2)
+        # Di is a diagonal matrix whose diagonal entries are the sums of Li columns
+        q1 = prob(np.around(null(L1-D1),3))
+        q2 = prob(np.around(null(L2-D2),3))
+        # qi is the strategy played at t+1 that satisfies the condition described
+        # in the paper.
+        Z2[np.random.choice(A1,p=q1),np.random.choice(A2,p=q2)] += 1
+        # Z2 += q1.reshape(2,1)*q2
+    print("nonpositive portion of player 1's Correlated Equilibrium matrix\n", L1)
+    print("nonpositive portion of player 2's Correlated Equilibrium matrix\n", L2)
+    return Z2/T
 
-        print("D2\n",D2)
-        print("null2\n", null(L2-D2))
-        print("q2\n",q2)
-        print()
-
+def NM(game, T):
+    # this is the procedure I proposed to find Nash Equilibrium.
+    G1 = g(game.u[0])
+    G2 = g(game.u[1])
+    Z2 = np.zeros(game.shape)
+    A1 = game.shape[0]
+    A2 = game.shape[1]
+    Z2[np.random.choice(A1),np.random.choice(A2)] += 1
+    for t in range(1,T+1):
+        MZ2 = marginal(Z2/t)
+        L1 = np.sum(MZ2*G1,axis=2)
+        L2 = np.sum(np.transpose(MZ2)*G2,axis=2)
+        L1[L1<0]=0
+        L2[L2<0]=0
+        # The calculation is similar to CRM except that here we use the product
+        # of the marginal distributions rather than the joint distribution.
+        D1 = np.diagflat(np.sum(L1,axis=0))
+        D2 = np.diagflat(np.sum(L2,axis=0))
+        q1 = prob(np.around(null(L1-D1),3))
+        q2 = prob(np.around(null(L2-D2),3))
         Z2[np.random.choice(A1,p=q1),np.random.choice(A2,p=q2)] += 1
         #Z2 += q1.reshape(2,1)*q2
-    return Z2/T
+    print("strategy for player 1: \n",np.sum(Z2, axis=1)[None]/t)
+    print("strategy for player 2: \n",np.sum(Z2, axis=0)[None]/t)
+    print("nonpositive portion of player 1's Nash matrix\n", L1)
+    print("nonpositive portion of player 2's Nash matrix\n", L2)
+    print()
+    return marginal(Z2/T)
 
 def main():
     np.random.seed(0)
-    game = Chicken()
-    print(CRM(game,100))
+    game = RPS3()
+    print("joint distribution (row player = player 1) \n", NM(game,100))
 
 main()
